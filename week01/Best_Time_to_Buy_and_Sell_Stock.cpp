c8fb8df2da11b75e49803482de6572aac58a2f11
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-06-20 20:58:46 +0600

    initial stage

---
+++ week01/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,27 @@
+/*Mirza Mohibul Hasan*/
+/*CSE-19@JUST*/
+#include <bits/stdc++.h>
+using namespace std;
+#define pi acos(-1)
+#define mod 1000000007
+#define ll long long
+#define li long ll
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define YES  cout << "YES\n"
+#define NO   cout << "NO\n"
+#define Yes  cout << "Yes\n"
+#define No   cout << "No\n"
+#define nl "\n"
+#define deb(x) cout << #x << "=" << x <<'\n'
+#define fast_run ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+ 
+int main()
+{
+    fast_run;
+    cout<<"Hello World"<<nl;
+    return 0;
+}
