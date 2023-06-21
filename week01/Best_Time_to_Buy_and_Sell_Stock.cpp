b2c0792cfe71ab423c7d23aed5ace5e35e05bd40
Mirza Mohibul Hasan <mirzamohibul618@.com>
2023-06-21 12:41:38 +0600

    buy sell added

--- week01/Best_Time_to_Buy_and_Sell_Stock.cpp
+++ week01/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,27 +1,19 @@
-/*Mirza Mohibul Hasan*/
-/*CSE-19@JUST*/
-#include <bits/stdc++.h>
-using namespace std;
-#define pi acos(-1)
-#define mod 1000000007
-#define ll long long
-#define li long ll
-#define ld long double
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define YES  cout << "YES\n"
-#define NO   cout << "NO\n"
-#define Yes  cout << "Yes\n"
-#define No   cout << "No\n"
-#define nl "\n"
-#define deb(x) cout << #x << "=" << x <<'\n'
-#define fast_run ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
- 
-int main()
-{
-    fast_run;
-    cout<<"Hello World"<<nl;
-    return 0;
-}
+//TC: O(N)
+//MC: O(N)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int lowestPrice = prices[0];
+    int profit = 0;
+    for (int i = 0; i< prices.size(); i++)
+    {
+        if(prices[i]<lowestPrice){
+            lowestPrice = prices[i];
+        }
+        if(prices[i]-lowestPrice > profit){
+            profit = prices[i]-lowestPrice;
+        }
+    }
+    return profit;
+    }
+};
